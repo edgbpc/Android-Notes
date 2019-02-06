229ee83bdf731a667aa0ed60ab8f5f0a9051fa81
ericgoodwin <edgbpc@mail.umsl.edu>
2019-02-06 17:27:26 -0600

    Initial Commit

---
+++ Android Notes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Android Notes
+//
+//  Created by Eric Goodwin on 2/6/19.
+//  Copyright © 2019 umsl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
